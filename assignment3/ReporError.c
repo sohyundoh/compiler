75d7cfd71f06f74017ff307754f43094d05d2e55
sohyundoh <lyny.doh@gmail.com>
2023-05-16 14:49:49 +0900

    Update ReporError.c

--- assignment3/ReporError.c
+++ assignment3/ReporError.c
@@ -1,7 +1,44 @@
-#include <stdio.h>
-//ø°∑Ø¥¬ ¡Ÿ(line)µµ «¡∏∞∆Æ «œ±‚
-yyerror(sb)
-char *sb;
-{
-	printf("%s\n",sb);
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"glob.h"
+
+/*
+ * global error print function
+ * ÏóêÎü¨ Ìè¨Îß∑Ïóê ÎßûÏ∂∞ ÌîÑÎ¶∞Ìä∏
+ * */
+void printError() {
+    printf("%13dfh", cLine);
+    printf("%13s", "**ERROR**");
+    printf("%13s", "");
+    printf("%s ", yytext);
+}
+
+/* printtoolongError
+    : identifier too longs*/
+void printtoolongError() {
+    printError();
+    printf("long identifier");
+}
+
+/* printillidError
+   : identifier illegal format */
+void printillidError() {
+    printError();
+    printf("illegal identifier");
+}
+
+/* printillspError
+   : illgal character/string */
+void printillspError() {
+    printError();
+    printf("illegal character");
+}
+
+/* printoverflowError
+   : overflow */
+void printoverflowError() {
+    printf("%-18s", "***error***");
+    printf("%-18s", "");
+    printf("OVERFLOW %s", yytext);
 }
